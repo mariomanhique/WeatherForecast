0f900257e0a926b77ce3044656ffe6bf0a88e185
Mario Manhique <79359430+MErnesto99@users.noreply.github.com>
2023-08-19 01:46:19 -0700

    Update Constants.kt

--- app/src/main/java/com/example/weatherforecast/utils/Constants.kt
+++ app/src/main/java/com/example/weatherforecast/utils/Constants.kt
@@ -2,5 +2,6 @@ package com.example.weatherforecast.utils
 
 object Constants {
     const val BASE_URL="https://api.openweathermap.org/"
-    const val ApiKey= "ed60fcfbd110ee65c7150605ea8aceea"
+    const val ApiKey= ""
+    //For the API Key please go to https://home.openweathermap.org/ to create your own.
 }
